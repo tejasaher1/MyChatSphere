3c050fc9bf32a9a840a811a1e3bb0a95f8b0b475
tejasaher1 <tejasaher9623@gmail.com>
2024-05-18 15:38:47 +0530

    Adding MongoDB cluster cloud URI

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,13 +1,35 @@
+// const mongoose = require('mongoose');
+
+// mongoose.connect('mongodb://127.0.0.1:27017/MyChatSphere');
+
+// const db = mongoose.connection;
+
+// db.on('error', console.log.bind(console,'error connecting to db'));
+
+// db.once('open', function(){
+//     console.log('Successfully connected to database!');
+// })
+
+// module.exports = db;
+
+
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://127.0.0.1:27017/MyChatSphere');
+// Connection URI for MongoDB Atlas
+const uri = 'mongodb+srv://tejasaher9623:5b3Q0cMcAGCKSD8O@production-cluster.hvonmpn.mongodb.net/MyChatSphere?retryWrites=true&w=majority';
+
+// Connect to MongoDB Atlas
+mongoose.connect(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 const db = mongoose.connection;
 
-db.on('error', console.log.bind(console,'error connecting to db'));
+db.on('error', console.log.bind(console, 'Error connecting to db'));
 
-db.once('open', function(){
-    console.log('Successfully connected to database!');
-})
+db.once('open', function() {
+  console.log('Successfully connected to database!');
+});
 
 module.exports = db;
