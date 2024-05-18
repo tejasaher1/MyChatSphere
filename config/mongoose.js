971ee1b139bd0810d75729b4f9ecf322426257e2
tejasaher1 <tejasaher9623@gmail.com>
2024-05-18 16:17:27 +0530

    changes

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,20 +1,7 @@
-// const mongoose = require('mongoose');
-
-// mongoose.connect('mongodb://127.0.0.1:27017/MyChatSphere');
-
-// const db = mongoose.connection;
-
-// db.on('error', console.log.bind(console,'error connecting to db'));
-
-// db.once('open', function(){
-//     console.log('Successfully connected to database!');
-// })
-
-// module.exports = db;
-
+require('dotenv').config();     // Load environment variables from .env file
 
 const mongoose = require('mongoose');
-require('dotenv').config(); // Load environment variables from .env file
+ 
 
 const uri = process.env.MONGODB_URI;
 
